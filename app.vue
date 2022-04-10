23b3d19cad977256084b3edc91b4d24ef0cfd660
Giancarlo Papa <giancarlo.papa@gmail.com>
2022-04-10 22:23:56 +0200

    init risebooclub with cloudflare wrangler workers

--- app.vue
+++ app.vue
@@ -2,11 +2,7 @@
   <div class="hero min-h-screen bg-base-200">
     <div class="text-center hero-content">
       <div class="max-w-md">
-        <h1 class="mb-5 text-5xl font-bold">Bompietro.com</h1>
-        <p class="mb-5">U Situ di Bompitrara</p>
-        <a href="https://github.com/gpx75/bompietro.com">
-          <button class="btn btn-primary">Ci stamu travagghiannu!</button>
-        </a>
+        <h1 class="mb-5 text-5xl font-bold">risebookclub.com</h1>
       </div>
     </div>
   </div>
