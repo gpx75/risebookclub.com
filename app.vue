a1acc00f0fad4a68c08f7e44927ea00db1e0b8fc
Giancarlo Papa <giancarlo.papa@gmail.com>
2022-04-12 09:35:55 +0200

    installed fonts and color palette

--- app.vue
+++ app.vue
@@ -2,8 +2,28 @@
   <div class="hero min-h-screen bg-base-200">
     <div class="text-center hero-content">
       <div class="max-w-md">
-        <h1 class="mb-5 text-5xl font-bold">risebookclub.com</h1>
+        <h1 class="mb-5 text-9xl font-bold text-primary">RISE</h1>
+        <h3 class="mb-5 text-3xl">Almost before we knew it, we had left the ground.</h3>
+        <div class="card m-auto w-96 bg-base-100 shadow-xl image-full">
+          <figure><img src="https://api.lorem.space/image/book?w=400&h=225" alt="book" /></figure>
+          <div class="card-body">
+            <p class="font-cursive text-5xl text-white">If a dog chews books whose books does he choose?</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
+<script setup lang="ts">
+useMeta({
+  meta: [
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1, maximum-scale=1",
+    },
+  ],
+  htmlAttrs: {
+    "data-theme": "riseTheme",
+  },
+});
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,38 @@
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        riseTheme: {
+          primary: "#25AFAA",
+
+          secondary: "#FDAAC7",
+
+          accent: "#F9DE73",
+
+          neutral: "#291334",
+
+          "base-100": "#FAF7F5",
+
+          info: "#3ABFF8",
+
+          success: "#6DDFDD",
+
+          warning: "#FFAB77",
+
+          error: "#FF7676",
+        },
+      },
+    ],
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Quicksand", "sans-serif"],
+        heading: ["Jeko-Medium"],
+        cursive: ["Shadows Into Light", "cursive"],
+      },
+    },
+  },
   darkMode: "class",
   content: ["./components/**/*.{vue,js}", "./composables/**/*.{js,ts}", "./layouts/**/*.vue", "./pages/**/*.vue", "./plugins/**/*.{js,ts}", "./utils/**/*.{js,ts}", "./app.vue"],
   plugins: [require("daisyui")],
